87f9b6eb365b27eefa2f249073cca4a7ed6c1813
Kilian <dankestmirror@hotmail.es>
2026-05-20 17:56:41 +0100

    seguridad

---
+++ auth/jwt.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from config import settings
+
+# Configuración de Passlib para encriptar contraseñas
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def obtener_password_encriptada(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verificar_password(password_plana: str, password_encriptada: str) -> bool:
+    return pwd_context.verify(password_plana, password_encriptada)
+
+def crear_token_acceso(datos: dict, expires_delta: Optional[timedelta] = None) -> str:
+    datos_a_encriptar = datos.copy()
+    if expires_delta:
+        tiempo_expiracion = datetime.now(timezone.utc) + expires_delta
+    else:
+        tiempo_expiracion = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    datos_a_encriptar.update({"exp": tiempo_expiracion})
+    token_jwt = jwt.encode(datos_a_encriptar, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return token_jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def obtener_usuario_actual(token: str = Depends(oauth2_scheme)) -> str:
+    credenciales_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="No se pudieron validar las credenciales",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Desencriptamos el token con nuestra clave secreta
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credenciales_exception
+        return email  # Devolvemos el email del usuario dueño del token
+    except JWTError:
+        raise credenciales_exception
